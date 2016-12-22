334de5ef879683c942c43cd260fe76f1a12d8932
Omar J. Rodríguez <omarjavier15@gmail.com>
2016-12-22 19:23:39 -0400

    Generate stubs

---
+++ FTF.Specs/Steps.cs
@@ -0,0 +1,28 @@
+﻿using TechTalk.SpecFlow;
+
+namespace FTF.Specs
+{
+    [Binding]
+    public class Steps
+    {
+        [Given(@"today is '(.*)'")]
+        public void TodayIs(string date)
+        {
+        }
+
+        [Given(@"I created a note \#(.*) with text '(.*)'")]
+        public void CreateNote(int id, string text)
+        {
+        }
+
+        [When(@"I retrieve the note \#(.*)")]
+        public void RetrieveNote(int id)
+        {
+        }
+
+        [Then(@"the note should match:")]
+        public void NoteShouldMatch(Table table)
+        {
+        }
+    }
+}
