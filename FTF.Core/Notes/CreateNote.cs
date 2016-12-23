e2673ace17d3663e266505b69bf861ad3aa2eb74
Omar J. Rodríguez <omarjavier15@gmail.com>
2016-12-23 11:23:40 -0400

    Tests passing

--- FTF.Core/Notes/CreateNote.cs
+++ FTF.Core/Notes/CreateNote.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using FTF.Core.Extensions;
 
 namespace FTF.Core.Notes
 {
@@ -34,6 +36,11 @@ namespace FTF.Core.Notes
             _saveChanges();
         }
 
-        private ICollection<Tag> ParseTags(string text) => new List<Tag> { new Tag { Name = "Buy"} };
+        private ICollection<Tag> ParseTags(string text) => 
+            text
+                .ParseTagNames()
+                .Distinct()
+                .Select(_ => new Tag { Name = _ })
+                .ToList();
     }
 }
