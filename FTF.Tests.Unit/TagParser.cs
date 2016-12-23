a9d81c228444c097871a239a6c3592bedad30dd0
Omar J. Rodríguez <omarjavier15@gmail.com>
2016-12-23 11:08:44 -0400

    Add pending test for tag parser

---
+++ FTF.Tests.Unit/TagParser.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace FTF.Tests.Unit
+{
+    public class TagParser
+    {
+        [Fact(Skip = "Pending")]
+        public void Pending()
+        {
+            
+        }
+    }
+}
