c24701ca775f4250e399b9dfcf46245e9689e219
Omar J. Rodríguez <omarjavier15@gmail.com>
2016-12-29 11:28:35 -0400

    Refactor: Extract SignUp.Handler

---
+++ FTF.Core/Auth/SignUp/Handler.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace FTF.Core.Auth.SignUp
+{
+    public class Handler
+    {
+        private readonly Action<User> _saveUser;
+
+        private readonly Action _saveChanges;
+
+        public Handler(
+            Action<User> saveUser, 
+            Action saveChanges)
+        {
+            _saveUser = saveUser;
+            _saveChanges = saveChanges;
+        }
+
+        public void SignUp(string userName)
+        {
+            _saveUser(new User
+            {
+                Name = userName
+            });
+
+            _saveChanges();
+        }
+    }
+}
--- FTF.Specs/Steps/AuthSteps.cs
+++ FTF.Specs/Steps/AuthSteps.cs
@@ -1,5 +1,6 @@
-﻿using System.Linq;
-using FTF.Core;
+﻿using System.Data.Entity;
+using System.Linq;
+using FTF.Core.Auth.SignUp;
 using TechTalk.SpecFlow;
 
 namespace FTF.Specs.Steps
@@ -17,12 +18,10 @@ namespace FTF.Specs.Steps
         [Given(@"I signup as '(.*)'")]
         public void SignUp(string userName)
         {
-            _context.Db.Users.Add(new User
-            {
-                Name = userName
-            });
-
-            _context.Db.SaveChanges();
+            new Handler(
+                saveUser: user => _context.Db.Users.Add(user), 
+                saveChanges: () => _context.Db.SaveChanges()
+            ).SignUp(userName);
         }
 
         [Given(@"I signin as '(.*)'")]
