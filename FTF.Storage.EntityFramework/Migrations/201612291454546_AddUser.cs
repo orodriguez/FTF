c79f183152dba2fd81f48d68c2e4fe329886d083
Omar J. Rodríguez <omarjavier15@gmail.com>
2016-12-29 10:56:35 -0400

    Add migration

---
+++ FTF.Storage.EntityFramework/Migrations/201612291454546_AddUser.cs
@@ -0,0 +1,26 @@
+namespace FTF.Storage.EntityFramework.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddUser : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Users",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Username = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Users");
+        }
+    }
+}
