1daaa459c9f2c37a3e89e7b2b631b204750f24da
Omar J. Rodríguez <omarjavier15@gmail.com>
2017-01-06 16:47:02 -0400

    Notes.CreateHandler uses SimpleInjector Remove Hooks because Context has the same life spam of an scenario

--- FTF.Specs/Context.cs
+++ FTF.Specs/Context.cs
@@ -1,16 +1,22 @@
 ﻿using System;
 using System.Data.Entity;
+using System.Linq;
+using FTF.Api.Actions.Notes;
 using FTF.Core.Delegates;
 using FTF.Core.Entities;
+using FTF.Core.Notes;
+using SimpleInjector;
 using DbContext = FTF.Storage.EntityFramework.DbContext;
+using FTF.Core.Extensions.Queriable;
+using SimpleInjector.Extensions.LifetimeScoping;
 
 namespace FTF.Specs
 {
-    public class Context
+    public class Context : IDisposable
     {
         private User _currentUser;
 
-        public DbContext Db { get; set; }
+        public DbContext Db => _container.GetInstance<DbContext>();
 
         public GetCurrentDate GetCurrentDate { get; set; }
 
@@ -30,9 +36,29 @@ namespace FTF.Specs
             set { _currentUser = value; }
         }
 
+        private readonly Container _container;
+
+        private readonly Scope _scope;
+
         public Context()
         {
             GetCurrentDate = () => DateTime.Now;
+            _container = new Container();
+            _container.Options.DefaultScopedLifestyle = new LifetimeScopeLifestyle();
+            _container.Register<Create>(() => _container.GetInstance<CreateHandler>().Create);
+            _container.Register<CreateHandler>();
+            _container.Register<GenerateNoteId>(() => _container.GetInstance<IQueryable<Note>>().NextId);
+            _container.Register(() => GetCurrentDate);
+            _container.Register<Save<Note>>(() => e => _container.GetInstance<DbContext>().Notes.Add(e));
+            _container.Register<SaveChanges>(() => _container.GetInstance<DbContext>().SaveChanges);
+            _container.Register<IQueryable<Tag>>(() => _container.GetInstance<DbContext>().Tags);
+            _container.Register<GetCurrentUser>(() => () => CurrentUser);
+            _container.Register<ValidateNote>(() => NoteValidator.Validate);
+            _container.Register<IQueryable<Note>>(() => _container.GetInstance<DbContext>().Notes);
+            _container.Register(() => new DbContext("name=FTF.Tests", new DropCreateDatabaseAlways<DbContext>()), Lifestyle.Scoped);
+
+            _scope = _container.BeginLifetimeScope();
+            Transaction = _container.GetInstance<DbContext>().Database.BeginTransaction();
         }
 
         public void StoreException(Action action)
@@ -45,6 +71,16 @@ namespace FTF.Specs
             {
                 Exception = e;
             }
+        }
+
+        public void Exec<T>(Action<T> action) where T : class => 
+            StoreException(() => action(_container.GetInstance<T>()));
+
+        public void Dispose()
+        {
+            Transaction.Rollback();
+            Db.Dispose();
+            _scope.Dispose();
         }
     }
 }
--- FTF.Specs/Steps/Steps.cs
+++ FTF.Specs/Steps/Steps.cs
@@ -1,12 +1,4 @@
 using System;
-using System.Linq;
-using FTF.Api.Actions.Notes;
-using FTF.Core.Delegates;
-using FTF.Core.Entities;
-using FTF.Core.Extensions.Queriable;
-using FTF.Core.Notes;
-using FTF.Storage.EntityFramework;
-using SimpleInjector;
 
 namespace FTF.Specs.Steps
 {
@@ -14,26 +6,15 @@ namespace FTF.Specs.Steps
     {
         protected Context Context;
 
-        private readonly Container _container = new Container();
+        
 
         public Steps(Context context)
         {
             Context = context;
 
-            _container.Register<Create>(() => _container.GetInstance<CreateHandler>().Create);
-            _container.Register<CreateHandler>();
-            _container.Register<GenerateNoteId>(() => _container.GetInstance<IQueryable<Note>>().NextId);
-            _container.Register(() => Context.GetCurrentDate);
-            _container.Register<Save<Note>>(() => e => _container.GetInstance<DbContext>().Notes.Add(e));
-            _container.Register<SaveChanges>(() => _container.GetInstance<DbContext>().SaveChanges);
-            _container.Register<IQueryable<Tag>>(() => _container.GetInstance<DbContext>().Tags);
-            _container.Register<GetCurrentUser>(() => () => Context.CurrentUser);
-            _container.Register<ValidateNote>(() => NoteValidator.Validate);
-            _container.Register<IQueryable<Note>>(() => _container.GetInstance<DbContext>().Notes);
-            _container.Register(() => new DbContext("name=FTF.Tests", new System.Data.Entity.DropCreateDatabaseAlways<DbContext>()));
+            
         }
 
-        protected void Exec<T>(Action<T> action) where T : class =>
-            Context.StoreException(() => action(_container.GetInstance<T>()));
+        protected void Exec<T>(Action<T> action) where T : class => Context.Exec<T>(action);
     }
 }
