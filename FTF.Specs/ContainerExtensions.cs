e47891c1a92d6896c4efc755a020b85f39301484
Omar J. Rodríguez <omarjavier15@gmail.com>
2017-01-07 16:04:42 -0400

    Organize registrations with comments

--- FTF.Specs/ContainerExtensions.cs
+++ FTF.Specs/ContainerExtensions.cs
@@ -19,36 +19,56 @@ namespace FTF.Specs
         public static void RegisterTypes(this Container c, GenerateNoteId generateNoteId, GetCurrentDate getCurrentDate, GetCurrentUser getCurrentUser, GetCurrentUserId getCurrentUserId, SetCurrentUser setCurrentUser)
         {
             c.Options.DefaultScopedLifestyle = new LifetimeScopeLifestyle();
-            c.Register<Create>(() => c.GetInstance<CreateHandler>().Create);
-            c.Register<CreateHandler>();
+
+            // Tests Specific Types
             c.Register(() => generateNoteId);
             c.Register(() => getCurrentDate);
-            c.Register<Save<Note>>(() => c.GetInstance<DbContext>().Notes.Add);
-            c.Register<SaveChanges>(() => c.GetInstance<DbContext>().SaveChanges);
-            c.Register<IQueryable<Tag>>(() => c.GetInstance<DbContext>().Tags);
             c.Register(() => getCurrentUser);
-            c.Register<ValidateNote>(() => NoteValidator.Validate);
-            c.Register<IQueryable<Note>>(() => c.GetInstance<DbContext>().Notes);
+            c.Register(() => getCurrentUserId);
+            c.Register(() => setCurrentUser);
+
+            // Actions delegates
+
+            // Auth
+            c.Register<SignUp>(() => c.GetInstance<Handler>().SignUp);
+            c.Register<SignIn>(() => c.GetInstance<FTF.Core.Auth.SignIn.Handler>().SignIn);
+
+            // Notes
+            c.Register<Create>(() => c.GetInstance<CreateHandler>().Create);
+            c.Register<Retrieve>(() => c.GetInstance<Queries>().Retrieve);
+            c.Register<Update>(() => c.GetInstance<UpdateHandler>().Update);
+            c.Register<Delete>(() => c.GetInstance<DeleteHandler>().Delete);
+
+            // Tags
+            c.Register<ListAll>(() => c.GetInstance<Core.Tags.Queries>().ListAll);
+            c.Register<ListJoint>(() => c.GetInstance<FTF.Core.Tags.Queries>().ListJoint);
+
+            // Actions implementations
+            c.Register<Handler>();
+            c.Register<Core.Auth.SignIn.Handler>();
+            c.Register<CreateHandler>();
+            c.Register<UpdateHandler>();
+            c.Register<DeleteHandler>();
+            c.Register<Queries>();
+            c.Register<Core.Tags.Queries>();
+
+            // Storage.EntityFramework
             c.Register(() => new DbContext("name=FTF.Tests", new System.Data.Entity.DropCreateDatabaseAlways<DbContext>()),
                 Lifestyle.Scoped);
-            c.Register<Delete>(() => c.GetInstance<DeleteHandler>().Delete);
-            c.Register<DeleteHandler>();
-            c.Register<Retrieve>(() => c.GetInstance<Queries>().Retrieve);
-            c.Register<Queries>();
-            c.Register(() => getCurrentUserId);
-            c.Register<SignUp>(() => c.GetInstance<Handler>().SignUp);
-            c.Register<Handler>();
-            c.Register<SignIn>(() => c.GetInstance<FTF.Core.Auth.SignIn.Handler>().SignIn);
-            c.Register<FTF.Core.Auth.SignIn.Handler>();
+            c.Register<SaveChanges>(() => c.GetInstance<DbContext>().SaveChanges);
+
+            // Save delegates
+            c.Register<Save<Note>>(() => c.GetInstance<DbContext>().Notes.Add);
             c.Register<Save<User>>(() => c.GetInstance<DbContext>().Users.Add);
+
+            // Queriables
+            c.Register<IQueryable<Tag>>(() => c.GetInstance<DbContext>().Tags);
+            c.Register<IQueryable<Note>>(() => c.GetInstance<DbContext>().Notes);
             c.Register<IQueryable<User>>(() => c.GetInstance<DbContext>().Users);
-            c.Register<SetCurrentUser>(() => setCurrentUser);
-            c.Register<ListAll>(() => c.GetInstance<FTF.Core.Tags.Queries>().ListAll);
-            c.Register<FTF.Core.Tags.Queries>();
-            c.Register<ListJoint>(() => c.GetInstance<FTF.Core.Tags.Queries>().ListJoint);
             c.Register<IQueryable<Tagging>>(() => c.GetInstance<DbContext>().Taggings);
-            c.Register<Update>(() => c.GetInstance<UpdateHandler>().Update);
-            c.Register<UpdateHandler>();
+
+            // Validators
+            c.Register<ValidateNote>(() => NoteValidator.Validate);
         }
     }
 }
