83c4adbe3e1ace04dd9625fd75dc16dcce40f733
Omar J. Rodríguez <omarjavier15@gmail.com>
2017-01-11 17:20:35 -0400

    Add test for list all tags

---
+++ FTF.Tests.XUnit/Tags/ListAllTest.cs
@@ -0,0 +1,45 @@
+﻿using System.Linq;
+using Xunit;
+
+namespace FTF.Tests.XUnit.Tags
+{
+    public class ListAllTest : ApplicationTest
+    {
+        [Fact]
+        public void _1NoteWithNewTag()
+        {
+            App.Notes.Create("#Read a book");
+
+            var tags = App.Tags.All();
+
+            var firstTag = tags.First();
+            Assert.Equal("Read", firstTag.Name);
+            Assert.Equal(1, firstTag.NotesCount);
+        }
+
+        [Fact]
+        public void _2NotesWithSameTag()
+        {
+            App.Notes.Create("#Buy cheese");
+            App.Notes.Create("#Buy nachos");
+
+            var tags = App.Tags.All();
+
+            var firstTag = tags.First();
+            Assert.Equal("Buy", firstTag.Name);
+            Assert.Equal(2, firstTag.NotesCount);
+        }
+
+        [Fact]
+        public void _1TagFromDifferentUser()
+        {
+            App.Notes.Create("#Buy cheese");
+            App.Auth.SignUp("anotheruser");
+            App.Auth.SignIn("anotheruser");
+
+            var tags = App.Tags.All();
+
+            Assert.Empty(tags);
+        }
+    }
+}
