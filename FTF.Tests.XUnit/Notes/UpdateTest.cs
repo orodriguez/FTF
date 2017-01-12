06dbb33b511271732941263c234203467b09d7a4
Omar J. Rodríguez <omarjavier15@gmail.com>
2017-01-12 18:05:08 -0400

    Add test Notes.UpdateTest.WithExistingTags

--- FTF.Tests.XUnit/Notes/UpdateTest.cs
+++ FTF.Tests.XUnit/Notes/UpdateTest.cs
@@ -35,6 +35,21 @@ namespace FTF.Tests.XUnit.Notes
         }
 
         [Fact]
+        public void WithExistingTags()
+        {
+            App.Notes.Create("Note with #Tag");
+
+            var noteId = App.Notes.Create("Note without tag");
+
+            App.Notes.Update(noteId, "Note update with #Tag");
+
+            var tags = App.Tags.All().ToList();
+
+            Assert.Equal(new[] { "Tag" }, tags.Select(t => t.Name));
+            Assert.Equal(new[] { 2 }, tags.Select(t => t.NotesCount));
+        }
+
+        [Fact]
         public void EmptyText()
         {
             var noteId = App.Notes.Create("Buy cheese");
