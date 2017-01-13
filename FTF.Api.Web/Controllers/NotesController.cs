80e7dbb8e11768a0bd4f7003b97bb933d4ae8fe4
Omar J. Rodríguez <omarjavier15@gmail.com>
2017-01-13 09:42:17 -0400

    Add NotesController

---
+++ FTF.Api.Web/Controllers/NotesController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+using FTF.Api.Requests.Notes;
+using FTF.Api.Responses;
+using FTF.Api.Services;
+
+namespace FTF.Api.Web.Controllers
+{
+    public class NotesController : ApiController
+    {
+        private readonly INotesService _notes;
+
+        public NotesController(INotesService notes)
+        {
+            _notes = notes;
+        }
+
+        public int Post(CreateRequest request) => _notes.Create(request);
+
+        public INote Retrieve(int noteId) => _notes.Retrieve(noteId);
+
+        public void Update(int id, UpdateRequest request) => _notes.Update(id, request);
+
+        public void Delete(int noteId) => _notes.Delete(noteId);
+
+        public IEnumerable<INote> All() => _notes.All();
+
+        public IEnumerable<INote> ByTag(string tagName) => _notes.ByTag(tagName);
+    }
+}
