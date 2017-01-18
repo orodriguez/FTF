abcbbab8ce3e39f238aaacfeab6a53c6eb6faadc
Omar J. Rodríguez <omarjavier15@gmail.com>
2017-01-18 16:56:11 -0400

    Extract Tagging Diff method

--- FTF.Core/Services/NotesService.cs
+++ FTF.Core/Services/NotesService.cs
@@ -111,51 +111,67 @@ namespace FTF.Core.Services
             {
                 noteToUpdate.Text = text;
 
-                var tagNamesInText = text.ParseTagNames();
+                var result = DiffTaggins(noteToUpdate, text);
 
-                var tagNamesAlreadyTagged = noteToUpdate.Tags.Select(t => t.Name).ToList();
-
-                var difference = tagNamesInText.Except(tagNamesAlreadyTagged);
-
-                var addedTags = difference
-                    .Where(t1 => tagNamesAlreadyTagged.All(t2 => t1 != t2))
-                    .ToList();
-
-                var existingTags = _db.Tags
-                    .Where(tag => addedTags.Any(tagName => tag.Name == tagName))
-                    .ToList();
-
-                var taggingsOfExistingTags = existingTags
-                    .Select(tag => new Tagging
-                    {
-                        Note = noteToUpdate,
-                        Tag = tag,
-                        CreationDate = _getCurrentTime()
-                    });
-
-                taggingsOfExistingTags
+                result.Added
                     .ToList()
-                    .ForEach(tagging => noteToUpdate.Taggings.Add(tagging));
+                    .ForEach(tagging => _db.Taggings.Add(tagging));
 
-                var tagsToCreate = addedTags.Except(existingTags.Select(t => t.Name));
-
-                var newTags = tagsToCreate.Select(tagName => new Tag
-                {
-                    Name = tagName,
-                    User = _getCurrentUser()
-                });
-
-                var taggingsOfNewTags = newTags.Select(tag => new Tagging
-                {
-                    Note = noteToUpdate,
-                    Tag = tag,
-                    CreationDate = _getCurrentTime(),
-                });
-
-                taggingsOfNewTags.ToList().ForEach(t => noteToUpdate.Taggings.Add(t));
+                result.Deleted
+                    .ToList()
+                    .ForEach(tagging => _db.Taggings.Remove(tagging));
             }
 
             _db.SaveChanges();
+        }
+
+        private DiffResult DiffTaggins(Note note, string text)
+        {
+            var tagNamesInText = text.ParseTagNames();
+
+            var existingTaggings = _db.Taggings
+                .Where(tagging => tagging.Note.Id == note.Id);
+
+            var existingTaggingsNames = existingTaggings.Select(tagging => tagging.Tag.Name);
+
+            var difference = tagNamesInText.Except(existingTaggingsNames);
+
+            var addedTags = difference
+                .Where(t1 => existingTaggingsNames.All(t2 => t1 != t2))
+                .ToList();
+
+            var existingTags = _db.Tags
+                .Where(tag => addedTags.Any(tagName => tag.Name == tagName))
+                .ToList();
+
+            var taggingsOfExistingTags = existingTags
+                .Select(tag => new Tagging
+                {
+                    Note = note,
+                    Tag = tag,
+                    CreationDate = _getCurrentTime()
+                });
+
+            var tagsToCreate = addedTags.Except(existingTags.Select(t => t.Name));
+
+            var newTags = tagsToCreate.Select(tagName => new Tag
+            {
+                Name = tagName,
+                User = _getCurrentUser()
+            });
+
+            var taggingsOfNewTags = newTags.Select(tag => new Tagging
+            {
+                Note = note,
+                Tag = tag,
+                CreationDate = _getCurrentTime(),
+            });
+
+            var taggingsToDelete = note.Taggings.Where(t => !tagNamesInText.Contains(t.Tag.Name));
+
+            var taggingsToAdd = taggingsOfNewTags.Concat(taggingsOfExistingTags);
+
+            return new DiffResult(taggingsToAdd, taggingsToDelete);
         }
 
         public void Delete(int id)
@@ -191,5 +207,18 @@ namespace FTF.Core.Services
 
             return notes.ToList().Select(n => new Responses.Note(n));
         }
-    }
+
+        private class DiffResult
+        {
+            public IEnumerable<Tagging> Added { get; }
+
+            public IEnumerable<Tagging> Deleted { get; }
+
+            public DiffResult(IEnumerable<Tagging> added, IEnumerable<Tagging> deleted)
+            {
+                Added = added;
+                Deleted = deleted;
+            }
+        }
+    }  
 }
